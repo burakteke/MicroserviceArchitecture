86022ae752d48f6b995bb521b0bab4d5b3a39cff
Burak Teke <burakteke37@gmail.com>
2023-11-04 04:39:45 +0300

    Catalog and models

---
+++ Catalog.API/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedTime { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+        public string Description { get; set; }
+        [BsonIgnore]
+        public Category Category { get; set; }
+    }
+}
