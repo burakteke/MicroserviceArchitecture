350e4753962ec4a836cd24e9a01d73553ce08d39
Burak Teke <burakteke37@gmail.com>
2023-11-11 01:30:07 +0300

    controllers

--- Services/Catalog/Catalog.API/Controllers/CategoryController.cs
+++ Services/Catalog/Catalog.API/Controllers/CategoryController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Catalog.API.Models;
+using Catalog.API.Repositories.Interfaces;
+using Microservice.Shared.Controllers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CategoryController : ControllerBase
+    public class CategoryController : CustomBaseController
     {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _categoryRepository.GetAll();
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _categoryRepository.GetById(id);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            var response = await _categoryRepository.Create(category);
+            return CreateActionResultInstance(response);
+        }
+
     }
 }
